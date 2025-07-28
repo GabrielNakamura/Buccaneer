7dcb4725b488f0a4d39bcddd384f398ea91d4bde
GabrielNakamura <gabriel.nakamura.souza@gmail.com>
2025-07-28 08:52:04 +0200

    fixing small error in richness data frame from clade richness function

--- R/clade_regional_richness.R
+++ R/clade_regional_richness.R
@@ -1,73 +1,55 @@
 #' Calculates regional clade richness for lineages
 #'
-#' @param df.TS.TE 
-#' @param time.slice 
-#' @param round.digits 
-#' @param species 
-#' @param TS 
-#' @param TE 
+#' @param df.TS.TE Data frame object containing at least four columns. Species names,
+#'     origination time, extinction time and a trait value for each species.
+#' @param time.slice Scalar indicating the time interval between consecutive time slices.
+#' @param round.digits Scalar indicating the precision of time slices.
+#' @param species Character indicating the name of the column of the data frame
+#'     containing the species name information.
+#' @param TS Character indicating the name of the columns of the data frame
+#'     containing the information on origination time.
+#' @param TE Character indicating the name of the column of the data frame
+#'     containing the information on extinction time.
 #'
-#' @return
+#' @return A data frame with richness information in each timeslice
 #' @export
 #'
 #' @examples
-regional_clade_richness <- 
+regional_clade_richness <-
   function(df.TS.TE,
-           time.slice, 
+           time.slice,
            round.digits = 1,
            species = "species",
            TS = "TS",
            TE = "TE"){
+
+
     df.TS.TE <- df.TS.TE[, c(species, TS, TE)]
-    
-    # Generating time intervals used to compute temporal coexistence
+    colnames(df.TS.TE) <- c("species", "TS", "TE")
+
+    # creating time slices
     seq_interval <- seq(from = max(df.TS.TE[, "TS"]), to = min(df.TS.TE[, "TE"]), by = -time.slice)
     seq_interval <- round(seq_interval, digits = round.digits)
-    
-    # defining species per bin and subsetting longevities data frame
-    df_sub_bin <- vector(mode = "list", length = length(seq_interval))
-    for (i in 1:length(seq_interval)){
-      # i = 1
-      df_sub_bin[[i]] <- df.TS.TE[which(df.TS.TE$TS >= seq_interval[i] & df.TS.TE$TE <= seq_interval[i]), ]
-    }
-    matrix_coex_bin <- vector(mode = "list", length = length(df_sub_bin))
-    
-    # list with species pairwise matrix
-    list_matrix_coex <- 
-      lapply(matrix_coex_bin, function(x){
-        matrix(0, nrow = length(df.TS.TE$species), ncol = length(df.TS.TE$species), 
-               dimnames = list(df.TS.TE$species, df.TS.TE$species)
-        )
+
+    # co-occurrence matrix
+    matrix_coex <-
+      aux_matrix_regional_coex(df.TS.TE, time.slice, round.digits = 1,
+                               species = "species",
+                               TS = "TS",
+                               TE = "TE")
+
+    # community composition matrix
+
+    spp_slice <-
+      lapply(matrix_coex, function(x){
+        names(which(rowSums(x) >= 1))
       })
-    
-    # pairwise matrix with co-occurrences
-    for(i in 1:length(list_matrix_coex)){
-      # i = 10
-      list_matrix_coex[[i]][match(df_sub_bin[[i]]$species, rownames(list_matrix_coex[[i]])), 
-                            match(df_sub_bin[[i]]$species, colnames(list_matrix_coex[[i]]))] <- 1
-    }
-    
-    # just naming columns with slice names
-    names(df_sub_bin) <- seq_interval
-    df_sub_bin2 <- 
-      lapply(names(df_sub_bin), function(name) {
-        df <- df_sub_bin[[name]]
-        df$time.slice <- name  # Add the name of the element as the new column
-        return(df)
-      })
-    
-    names(df_sub_bin2) <- names(df_sub_bin)
-    
-    # calculating lineage metrics 
-    
-    list_richness <- 
-      lapply(df_sub_bin2, function(x){
-        length(unique(x$species))
-      })
-    
+
+    nspp_slice <- lapply(spp_slice, function(x) length(x))
+
     # dataframe with richness
-    df_richness_slice <- data.frame(richness = unlist(list_richness), time.slice = names(df_sub_bin))
-    
+    df_richness_slice <- data.frame(richness = unlist(nspp_slice), time.slice = seq_interval)
+
     return(df_richness_slice)
-    
+
   }
